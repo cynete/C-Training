0513e5bbf6b10d1b74798f1033ba2656124c99b4
cynete <frostfire@live.com>
2024-05-05 13:04:47 +0530

    Serilog updated

--- SeriLogTraining/SeriLogTraining/CustomJsonFormatter.cs
+++ SeriLogTraining/SeriLogTraining/CustomJsonFormatter.cs
@@ -8,32 +8,19 @@ namespace SerilogTraining
     {
         public void Format(LogEvent logEvent, TextWriter output)
         {
-            //var log = new
-            //{
-            //    Timestamp = logEvent.Timestamp,
-            //    Level = logEvent.Level,
-            //    Message = logEvent.RenderMessage(),
-            //    Exception = logEvent.Exception?.ToString()
-            //};
-            //output.Write(SerializeJson(log));
-
             var splunkLogRecord = new SplunkLogModel()
             {
 
                 Timestamp = logEvent.Timestamp,
                 Level = logEvent.Level.ToString(),
                 Message = logEvent.RenderMessage(),
-                Exception = logEvent.Exception?.ToString()
+                ExceptionMessage = logEvent.Exception?.Message??"",
+                ExceptionSource = logEvent.Exception?.Source??"",
+                ExceptionStackTrace = logEvent.Exception?.StackTrace ?? "",
+                ExceptionInnerException = logEvent.Exception?.InnerException?.ToString()??"",
             };
 
-            output.WriteLine(SerializeJson(splunkLogRecord));
-        }
-
-        private string SerializeJson(object obj)
-        {
-            // Here you can use your preferred JSON serialization library
-            // For simplicity, I'm using Newtonsoft.Json in this example
-            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            output.WriteLine(JsonConvert.SerializeObject(splunkLogRecord));
         }
     }
 
@@ -42,6 +29,9 @@ namespace SerilogTraining
         [JsonProperty(PropertyName = "@Timestamp")] public DateTimeOffset Timestamp { get; set; }
         [JsonProperty(PropertyName = "@Level")] public string Level { get; set; }
         [JsonProperty(PropertyName = "@Message")] public string Message { get; set; }
-        [JsonProperty(PropertyName = "@Exception")] public string? Exception { get; set; }
+        [JsonProperty(PropertyName = "@ExceptionMessage")] public string? ExceptionMessage { get; set; }
+        [JsonProperty(PropertyName = "@ExceptionSource")] public string? ExceptionSource { get; set; }
+        [JsonProperty(PropertyName = "@ExceptionStackTrace")] public string? ExceptionStackTrace { get; set; }
+        [JsonProperty(PropertyName = "@ExceptionInnerException")] public string? ExceptionInnerException { get; set; }
     }
 }
--- SeriLogTraining/SeriLogTraining/Program.cs
+++ SeriLogTraining/SeriLogTraining/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Serilog;
 using Serilog.Events;
-using Serilog.Formatting.Compact;
 using SerilogTraining;
 
 namespace SeriLogTraining
@@ -11,14 +10,15 @@ namespace SeriLogTraining
         {
             Console.WriteLine("Start of program");
 
-            string LogOutputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}] [{Level:u4}] {@Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Message:lj}{NewLine}{Exception}";
+            string LogOutputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}] [{Level:u4}] {Message:lj}{NewLine}{Exception}";
 
             //static logger
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
+
                 .Enrich.WithProperty("test", "enriched message")
                 .Destructure.ByTransforming<LogModel>(r => new { Id = r.Id, Name = r.Name })
-                .WriteTo.File("log.txt", outputTemplate: LogOutputTemplate, rollingInterval: RollingInterval.Minute, retainedFileCountLimit: 3, shared:true)
+                .WriteTo.File("log.txt", outputTemplate: LogOutputTemplate, rollingInterval: RollingInterval.Minute, retainedFileCountLimit: 3, shared: true)
                 .WriteTo.Console(outputTemplate: LogOutputTemplate)
                 .CreateLogger();
 
@@ -29,12 +29,12 @@ namespace SeriLogTraining
                 .CreateLogger();
 
             Console.WriteLine("\nLogger1 >>>>>>>>>>>>>>>>>>>");
-            Log.Information("Test information message {{{test}}} - log");
-            Log.Information("Test - display object {LogModel}- log");
-            Log.Information("Test - display object {LogModel}- log", new LogModel());
-            Log.Information("Test - display object {Id}- log", new LogModel() { Id = 1, Name = "JP", Department = "CSE" });
-            Log.Information("Test - display object {@LogModel}- log", new LogModel() { Id = 1, Name = "JP", Department = "CSE" });
-            Log.Information("Test - display object {$LogModel}- log", new LogModel() { Id = 1, Name = "JP", Department = "CSE" });
+            Log.Information("Tes1t information message {test} - log");
+            Log.Information("Test2 - display object {LogModel} - log");
+            Log.Information("Test3 - display object {LogModel} - log", new LogModel());
+            Log.Information("Test4 - display object {Id} - log", (new LogModel() { Id = 1, Name = "JP", Department = "CSE" }).Id);
+            Log.Information("Test5 - display object {@LogModel} - log", new LogModel() { Id = 1, Name = "JP", Department = "CSE" });
+            Log.Information("Test6 - display object {$LogModel} - log", new LogModel() { Id = 1, Name = "JP", Department = "CSE" });
             Log.Information("{DateTime}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff zzz"));
             Log.Information("current time is {@Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}");
             Log.Warning("This is warning");
@@ -46,14 +46,35 @@ namespace SeriLogTraining
             log2.Information("Test information message - log 2");
 
             Console.WriteLine("\nLogger3 >>>>>>>>>>>>>>>>>>>");
-            Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Verbose()
-            .WriteTo.File(new CustomJsonFormatter(),  "log.json", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7, shared: true)
-            .CreateLogger();
+            string ConsoleLogOutputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}] [{Level:u4}] {Message:lj}{NewLine}{Exception}";
+            bool LogToSplunk = true;
 
-            Log.Information("This is a log message.");
+            LoggerConfiguration loggerConfiguration = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            //.WriteTo.Console(new CustomJsonFormatter(), restrictedToMinimumLevel: LogEventLevel.Verbose);
+            .WriteTo.Console(outputTemplate: ConsoleLogOutputTemplate, restrictedToMinimumLevel: LogEventLevel.Information);
+
+            if (LogToSplunk)
+                loggerConfiguration.WriteTo.File(new CustomJsonFormatter(), "log.json",
+                                                rollingInterval: RollingInterval.Day,
+                                                retainedFileCountLimit: 7,
+                                                shared: true);
+
+            Log.Logger = loggerConfiguration.CreateLogger();
+
+            Log.Debug("Debug message 1");
+            Log.Verbose("Verbose message 1");
+            Log.Information("Information message 1 >> RequestID = {RequestID}", 1234);
+            Log.Information("Information message 2");
+            Log.Verbose("Verbose message 2");
+            Log.Debug("Debug message 2");
+
+            try { throw new ArgumentException("Hello"); }
+            catch (Exception e) { Log.Error(e, "Exception caught"); }
+
 
             Log.CloseAndFlushAsync();
+            log2.Dispose();
             Console.WriteLine("end of program");
         }
     }
