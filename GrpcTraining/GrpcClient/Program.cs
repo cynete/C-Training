39cdf9d017a7679c60594a453e9f91a4d9951f3c
cynete <frostfire@live.com>
2024-06-15 17:52:51 +0530

    GPRC training

---
+++ GrpcTraining/GrpcClient/Program.cs
@@ -0,0 +1,87 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System.Dynamic;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start of program");
+
+            GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7052");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            HelloRequest request = new HelloRequest() { Name = "JP" };
+            HelloReply response = client.SayHello(request);
+            Console.WriteLine($"Reply received = {response}");
+
+            HelloRequest request1 = new HelloRequest() { Name = "Prakash" };
+            HelloReply response1 = client.SayHello(request1);
+            Console.WriteLine($"Reply received = {response1}");
+            Console.WriteLine("-----------------------------------------------");
+
+            var EmployeeIdReq = new EmployeeIdRequest() { Id = 3 };
+            EmployeeDetailResponse response2 = client.GetEmployeeByID(EmployeeIdReq);
+            Console.WriteLine($"ID = {response2.Id}");
+            Console.WriteLine($"Name = {response2.Name}");
+            Console.WriteLine($"Age = {response2.Age}");
+            Console.WriteLine($"City = {response2.City}");
+            Console.WriteLine("-----------------------------------------------");
+
+            // Repeated response >> sends multiple response like array/list
+            var AllEmployeeResponse = client.GetAllEmployees(new Empty());
+            foreach (var employee in AllEmployeeResponse.EmployeeDetail)
+            {
+                Console.WriteLine($"ID = {employee.Id}");
+                Console.WriteLine($"Name = {employee.Name}");
+                Console.WriteLine($"Age = {employee.Age}");
+                Console.WriteLine($"City = {employee.City}");
+                Console.WriteLine("=====================");
+            }
+            Console.WriteLine("-----------------------------------------------");
+
+            //Stream >> sends reponse like a stream continuously
+            ReadStreamData();
+
+
+
+            Console.WriteLine("End of program");
+
+            Console.ReadLine();
+        }
+
+        static async void ReadStreamData()
+        {
+            try
+            {
+
+                GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7052");
+
+                var client = new Greeter.GreeterClient(channel);
+
+                Console.WriteLine("Stream Started.");
+                var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+                using var streamingCall = client.GetAllEmployeesAsStream(new Empty(), cancellationToken: cancellationToken.Token);
+
+                await foreach (var employee in streamingCall.ResponseStream.ReadAllAsync(cancellationToken: cancellationToken.Token))
+                {
+                    Console.WriteLine(employee);
+
+                    //if (employee.Name == "Emp3")
+                    //    cancellationToken.Cancel();
+                }
+
+                Console.WriteLine("Stream completed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occured = {ex}");
+            }
+
+        }
+    }
+}
---
+++ GrpcTraining/GrpcServer/Services/GreeterService.cs
@@ -0,0 +1,96 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcServer;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace GrpcServer.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private List<Employee> AllEmployees = new List<Employee>() {
+        new Employee() { Id = 1,Name = "Emp1", Age=10,City="Loc1"},
+        new Employee() { Id = 2,Name = "Emp2", Age=20,City="Loc2"},
+        new Employee() { Id = 3,Name = "Emp3", Age=30,City="Loc3"},
+        new Employee() { Id = 4,Name = "Emp4", Age=40,City="Loc4"},
+        new Employee() { Id = 5,Name = "Emp5", Age=50,City="Loc5"},
+        new Employee() { Id = 6,Name = "Emp6", Age=60,City="Loc6"}
+            };
+
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"This is information ####### request received from {request.Name}");
+            _logger.LogInformation(context.ToString());
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+
+        public override Task<EmployeeDetailResponse> GetEmployeeByID(EmployeeIdRequest request, ServerCallContext context)
+        {
+            var employee = AllEmployees.Where(e => e.Id == request.Id).FirstOrDefault();
+
+            if (employee != null)
+                return Task.FromResult(new EmployeeDetailResponse
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Age = employee.Age,
+                    City = employee.City
+                });
+            else
+                return Task.FromResult(new EmployeeDetailResponse
+                {
+                    Id = 0,
+                    Name = "",
+                    Age = 0,
+                    City = ""
+                });
+        }
+
+        // Repeated response >> sends multiple response like array/list
+        public override Task<AllEmployeeDetailsResponse> GetAllEmployees(Empty request, ServerCallContext context)
+        {
+            var respones = new AllEmployeeDetailsResponse();
+
+            foreach (var employee in AllEmployees)
+            {
+                respones.EmployeeDetail.Add(new EmployeeDetailResponse
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Age = employee.Age,
+                    City = employee.City
+                });
+            }
+
+            return Task.FromResult(respones);
+        }
+
+
+        //Stream >> sends reponse like a stream continuously
+        public override async Task GetAllEmployeesAsStream(Empty request, IServerStreamWriter<EmployeeDetailResponse> streamWriter, ServerCallContext context)
+        {
+
+            foreach (var employee in AllEmployees)
+            {
+                await streamWriter.WriteAsync(new EmployeeDetailResponse
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Age = employee.Age,
+                    City = employee.City
+                });
+                await Task.Delay(3000);
+            }
+
+            //Thread.Sleep(1000);
+        }
+    }
+}
